63f7e3a2f746f28f8cf6e06fb4dbda204cf3b47a
NkCoder5 <nandhakumar4115@gmail.com>
2025-09-16 08:09:30 +0530

    name, tab pic

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -129,7 +129,7 @@ const Portfolio = () => {
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between items-center py-4">
                         <div className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-                            Nandha Kumar S
+                            Nandha kumar S
                         </div>
                         {/* Desktop Menu */}
                         <div className="hidden md:flex space-x-8">
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as Hero } from './Hero';
-export { default as About } from './About';
-export { default as Projects } from './Projects';
-export { default as Experience } from './Experience';
-export { default as Contact } from './Contact'; 
